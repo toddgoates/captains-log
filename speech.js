940605333b91378f611382e6252139bc6b0e31bb
Todd Goates <toddgoates@gmail.com>
2019-03-21 21:46:12 -0600

    Initial commit

---
+++ speech.js
@@ -0,0 +1,33 @@
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement("p");
+const words = document.querySelector(".words");
+words.appendChild(p);
+
+recognition.addEventListener("result", e => {
+  const transcript = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join("");
+
+  p.textContent = transcript;
+  if (e.results[0].isFinal) {
+    p = document.createElement("p");
+    words.appendChild(p);
+  }
+
+  /*
+    // Do something with with a specific phrase...
+    if (transcript.includes('get the weather')) {
+      console.log('Getting weather with API');
+    }
+  */
+});
+
+recognition.addEventListener("end", recognition.start);
+
+recognition.start();
